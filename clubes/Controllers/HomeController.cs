b78634837212bf3f017af65fac297a4e753d9c56
guilhermebrunelli <160548160+guilhermebrunelli@users.noreply.github.com>
2024-03-27 14:49:20 -0300

    Alterado o Controller

--- clubes/Controllers/HomeController.cs
+++ clubes/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using clubes.Models;
 
@@ -15,12 +16,41 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        List<Clube> clubes = GetClubes();
+        List<Pais> paises = GetPaises();
+        ViewData["Paises"] = paises;
+        return View(clubes);
     }
 
-    public IActionResult Privacy()
+    public IActionResult Details(int id)
     {
-        return View();
+        List<Clube> clubes = GetClubes();
+        List<Pais> paises = GetPaises();
+        DetailsVM details = new() {
+            Paises = paises,
+            Atual = clubes.FirstOrDefault(p => p.Numero == id),
+            Anterior = clubes.OrderByDescending(p => p.Numero).FirstOrDefault(p => p.Numero < id),
+            Proximo = clubes.OrderBy(p => p.Numero).FirstOrDefault(p => p.Numero > id),
+        };
+        return View(details);
+    }
+
+    private List<Clube> GetClubes()
+    {
+        using (StreamReader leitor = new("Data\\clubes.json"))
+        {
+            string dados = leitor.ReadToEnd();
+            return JsonSerializer.Deserialize<List<Clube>>(dados);
+        }
+    }
+
+        private List<Pais> GetPaises()
+    {
+        using (StreamReader leitor = new("Data\\paises.json"))
+        {
+            string dados = leitor.ReadToEnd();
+            return JsonSerializer.Deserialize<List<Pais>>(dados);
+        }
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
